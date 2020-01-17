53c5d93588a076f2590c91507781f12399b78357
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-01-17 12:45:31 +0100

    Ajout de la cible de test.

---
+++ Swift-RPN/RPN/RPN.swift
@@ -0,0 +1,11 @@
+//
+//  RPN.swift
+//  RPN
+//
+//  Created by BARDON Rémi on 17/01/2020.
+//  Copyright © 2020 BARDON Rémi. All rights reserved.
+//
+
+import Foundation
+
+class RPN {}
---
+++ Swift-RPN/RPNTests/RPNTests.swift
@@ -0,0 +1,16 @@
+//
+//  RPNTests.swift
+//  RPNTests
+//
+//  Created by BARDON Rémi on 17/01/2020.
+//  Copyright © 2020 BARDON Rémi. All rights reserved.
+//
+
+import XCTest
+@testable import RPN
+
+class RPNTests: XCTestCase {
+
+    let rpn = RPN()
+
+}
