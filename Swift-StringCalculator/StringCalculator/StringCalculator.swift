aedaa30d19bd43b6a6279d9d4f0f14c67fc6e379
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-02-19 15:40:59 +0100

    Ajout du test "1.1,2.2" returns "3.3"

--- Swift-StringCalculator/StringCalculator/StringCalculator.swift
+++ Swift-StringCalculator/StringCalculator/StringCalculator.swift
@@ -10,10 +10,24 @@ import Foundation
 
 class StringCalculator {
 	
+	typealias Number = Double
+	
 	func add(numbersIn input: String) -> String {
-		guard let _ = Float(input) else { return "0" }
-		
-		return input
+		guard let numbers: [Number] = {
+			if input.contains(",") {
+				return input.split(separator: ",").map { Number($0)! }
+			} else if let number = Number(input) {
+				return [number]
+			}
+			return nil
+		}(), !numbers.isEmpty else { return "0" }
+		return String(format: "%g", sum(numbers))
+	}
+	
+	private func sum(_ sequence: [Number]) -> Number {
+		var res: Number = 0
+		sequence.forEach { res += $0 }
+		return res
 	}
 	
 }
--- Swift-StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ Swift-StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -24,6 +24,7 @@ class StringCalculatorTests: XCTestCase {
         let testSuite = XCTestSuite(name: NSStringFromClass(self))
 		addTests("", expectedResult: "0", toTestSuite: testSuite)
 		addTests("1", expectedResult: "1", toTestSuite: testSuite)
+		addTests("1.1,2.2", expectedResult: "3.3", toTestSuite: testSuite)
         return testSuite
     }
 	
@@ -37,7 +38,7 @@ class StringCalculatorTests: XCTestCase {
         }
     }
 
-    public func testCompute() {
+    public func testAdd() {
 		XCTAssertEqual(calculator.add(numbersIn: input), expectedResult)
     }
 
